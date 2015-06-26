c0dc944674a515bf70ff1586768bf47b0b3dfd03
Oren <orengolan@gmail.com>
2015-06-25 22:49:20 -0700

    OMG STATE

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var React = require('react');
 
 // https://facebook.github.io/react/docs/top-level-api.html
+var state = {x: 1};
+
 var App = React.createClass({
   render: function () {
-    return React.DOM.div(null, 'Hello World!')
+    return React.DOM.div(null, 'Hello World! ' + this.props.x)
   }
 });
 
 var elem = document.querySelector('body');
-React.render(React.createElement(App), elem);
+React.render(React.createElement(App, state), elem);
